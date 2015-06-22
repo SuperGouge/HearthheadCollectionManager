4cbc8c3ff7f29ca7a21f474a0e1f2deae5ea905f
SuperGouge <SuperGouge@users.noreply.github.com>
2015-06-23 01:50:26 +0200

    Added modal dialog when importing

--- HearthheadCollectionManager.user.js
+++ HearthheadCollectionManager.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/SuperGouge
 // @homepageURL https://github.com/SuperGouge/HearthheadCollectionManager
 // @match       http://*.hearthhead.com/collection
-// @version     1.0.1
+// @version     1.0.2
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @require     https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js
 // @grant       none
@@ -11,90 +11,112 @@
 
 this.$ = this.jQuery = jQuery.noConflict(true);
 
-$("head").append (
-    '<link href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/vader/jquery-ui.min.css" rel="stylesheet" type="text/css">'
-);
-
 const COLLECTABLE_SETS = [3, 13];
 
+$("<link/>", { href: "//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/vader/jquery-ui.min.css", rel: "stylesheet", type: "text/css" }).appendTo("head");
+var $textarea = $("<textarea/>", { css: { width: "100%", height: "100%", boxSizing: "border-box", resize: "none" } });
+var $dialog = $("<form/>", { css: { height: "100%" } }).append($textarea).dialog({
+    autoOpen: false,
+    modal: true,
+    minWidth: 480,
+    minHeight: 360
+}).on("dialogopen", function() {
+    $dialog.height($dialog.height());
+});
+
 $("#viewtoggle").before(
-    $("<a/>", { text: "Export", href: "#", class: "btn btn-site btn-small", css: { marginLeft: "5px", float: "right" } }).click(function(event) {
+    $("<a/>", { text: "Export", href: "#", "class": "btn btn-site btn-small" }).click(function(event) {
         event.preventDefault();
         var idString = "";
         $.each(HearthstoneCollectionUpdates.userCards, function(id, card) {
             idString += g_hearthstone_cards[id].image + ":" + card.normal + ":" + card.golden + ";";
         });
-        var dialog = '<div id=dialogexport><form><input type="text" name="export" id="export" style="width: 300px;" value="' + idString + '" /></form></div>';
-        $("body").append(dialog);
-        $("#dialogexport").prop("title", "Copy this text to clipboard:");
-        $("#dialogexport").dialog({
-            modal: true,
-            minWidth: 360,
-            buttons: {
-                OK: function() {
-                    $( this ).dialog( "close" );
-                }
+        $textarea.val(idString);
+        $textarea.prop("readonly", true);
+        $dialog.dialog("option", "title", "Copy this text to clipboard:");
+        $dialog.dialog("option", "buttons", {
+            OK: function() {
+                $(this).dialog("close");
             }
         });
+        $dialog.dialog("option", "open", function() {
+            $textarea.select();
+        });
+        $dialog.dialog("open");
     })
 ).before(
-    $("<a/>", { text: "Import", href: "#", class: "btn btn-site btn-small", css: { marginLeft: "5px", float: "right" } }).click(function(event) {
+    $("<a/>", { text: "Import", href: "#", "class": "btn btn-site btn-small", css: { marginLeft: "5px" } }).click(function(event) {
         event.preventDefault();
-        var idString = prompt("Paste ID string to import:");
-        if (!idString) {
-            return;
-        }
-        if (!/(\d+:[0-2]:[0-2];)+/.test(idString = idString.trim())) {
-            return alert("Malformed ID string");
-        }
-
-        var importedCards = {};
-        $.each(idString.split(";"), function(index, card) {
-            if (card) {
-                var split = card.split(":");
-                var id = cards[split[0]].id;
-                importedCards[id] = {
-                    id: id,
-                    normal: split[1],
-                    golden: split[2]
-                };
-            }
-        });
-
-        $.each(HearthstoneCollectionUpdates.userCards, function(id, card) {
-            if (importedCards[id]) {
-                if (card.normal != importedCards[id].normal || card.golden != importedCards[id].golden) {
-                    HearthstoneCollectionUpdates.savedCardChanges[id] = importedCards[id];
-                }
-                delete importedCards[id];
-            } else {
-                if ((card.normal != 0 || card.golden != 0) && $.inArray(g_hearthstone_cards[id].set, COLLECTABLE_SETS) != -1) {
-                    HearthstoneCollectionUpdates.savedCardChanges[id] = {
-                        id: id,
-                        normal: 0,
-                        golden: 0
-                    };
+        $textarea.val("");
+        $textarea.prop("readonly", false);
+        $dialog.dialog("option", "title", "Paste ID string to import:");
+        $dialog.dialog("option", "buttons", {
+            OK: function() {
+                var idString = $textarea.val().trim();
+                if (!/(\d+:[0-2]:[0-2];)+/.test(idString)) {
+                    $textarea.css("border-color", "red");
+                } else {
+                    setTimeout(function() {
+                        importCollection(idString);
+                    }, 0);
+                    $(this).dialog("close");
                 }
             }
         });
-        $.each(importedCards, function(id, card) {
-            HearthstoneCollectionUpdates.savedCardChanges[id] = card;
+        $dialog.dialog("option", "close", function() {
+            $textarea.css("border-color", "");
         });
-
-        $.each(HearthstoneCollectionUpdates.savedCardChanges, function(id, card) {
-            var $card = $(".cardline:has(div[rel='" + id + "'])");
-            $card.find(".buttons:not(.gold)>div").each(function() {
-                var $this = $(this);
-                $this.attr("class", $this.attr("cardcount") == card.normal ? "in" : "");
-            });
-            $card.find(".buttons.gold>div").each(function() {
-                var $this = $(this);
-                $this.attr("class", $this.attr("cardcount") == card.golden ? "in" : "");
-            });
-        });
-        HearthstoneCollectionUpdates.sendChanges(false);
+        $dialog.dialog("open");
     })
 );
+
+function importCollection(idString) {
+    var importedCards = {};
+    $.each(idString.split(";"), function(index, card) {
+        if (card) {
+            var split = card.split(":");
+            var id = cards[split[0]].id;
+            importedCards[id] = {
+                id: id,
+                normal: split[1],
+                golden: split[2]
+            };
+        }
+    });
+
+    $.each(HearthstoneCollectionUpdates.userCards, function(id, card) {
+        if (importedCards[id]) {
+            if (card.normal != importedCards[id].normal || card.golden != importedCards[id].golden) {
+                HearthstoneCollectionUpdates.savedCardChanges[id] = importedCards[id];
+            }
+            delete importedCards[id];
+        } else {
+            if ((card.normal != 0 || card.golden != 0) && $.inArray(g_hearthstone_cards[id].set, COLLECTABLE_SETS) != -1) {
+                HearthstoneCollectionUpdates.savedCardChanges[id] = {
+                    id: id,
+                    normal: 0,
+                    golden: 0
+                };
+            }
+        }
+    });
+    $.each(importedCards, function(id, card) {
+        HearthstoneCollectionUpdates.savedCardChanges[id] = card;
+    });
+
+    $.each(HearthstoneCollectionUpdates.savedCardChanges, function(id, card) {
+        var $card = $(".cardline:has(div[rel='" + id + "'])");
+        $card.find(".buttons:not(.gold)>div").each(function() {
+            var $this = $(this);
+            $this.attr("class", $this.attr("cardcount") == card.normal ? "in" : "");
+        });
+        $card.find(".buttons.gold>div").each(function() {
+            var $this = $(this);
+            $this.attr("class", $this.attr("cardcount") == card.golden ? "in" : "");
+        });
+    });
+    HearthstoneCollectionUpdates.sendChanges(false);
+}
 
 var cards = {};
 $.each(g_hearthstone_cards, function(id, card) {
